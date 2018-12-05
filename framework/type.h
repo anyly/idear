e7c304c522239d2d05c68a17f4902b90746c238c
tony <z_yg@winning.com.cn>
2018-12-05 17:03:17 +0800

    type.h

--- framework/type.h
+++ framework/type.h
@@ -1,6 +1,14 @@
 #ifndef TYPE_H_INCLUDED
 #define TYPE_H_INCLUDED
 
-struct
+struct Identifier {
+    char* key;
+    int type;
+    void* mem;
+};
+
+struct Object {
+    Identifier *property;
+};
 
 #endif // TYPE_H_INCLUDED
