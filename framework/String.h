95bd9fc7410eb18b873a2b73a7b22775f1a2231f
tony <z_yg@winning.com.cn>
2018-12-07 17:05:14 +0800

    string

--- framework/String.h
+++ framework/String.h
@@ -8,4 +8,11 @@ struct String {
     char *charArray;
 };
 
+String String@append(String thisObject, String...stringX);
+
+int String@indexOf(String thisObject, String searchvalue, int fromindex);
+
+String String@substr(String thisObject, int start, int length);
+
+String String@substring(String thisObject, int start, int stop);
 #endif // STRING_H_INCLUDED
